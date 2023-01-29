faf865eeda72c101cc8021129f10af42c188fbda
krzysiek-banan <84676694+krzysiek-banan@users.noreply.github.com>
2023-01-29 14:43:25 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PierwiastekRownania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
