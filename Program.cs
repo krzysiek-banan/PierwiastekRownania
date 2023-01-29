1824c1be28540411c77e79f0944b32d1d4768737
krzysiek-banan <84676694+krzysiek-banan@users.noreply.github.com>
2023-01-29 16:29:56 +0100

    final updated

--- Program.cs
+++ Program.cs
@@ -9,24 +9,24 @@
             var discriminant = b * b - 4 * a * c;
             if (discriminant < 0)
             {
-                return Tuple.Create(0.0, 0.0);
+                return Tuple.Create(double.NaN, double.NaN);
             }
             else if (discriminant == 0)
             {
-            double delta = -b / (2 * a);
-                return Tuple.Create(delta,delta );
+            double pierw = -b / (2 * a);
+                return Tuple.Create(pierw, double.NaN);
             }
             else
             {
                 var sqrtDicriminant = Math.Sqrt(discriminant);
-                return Tuple.Create((-b + sqrtDicriminant) / (2 * a), (-b - sqrtDicriminant) / (2 * a));
+                return Tuple.Create((-b - sqrtDicriminant) / (2 * a), (-b + sqrtDicriminant) / (2 * a));
             }
         }
         static void Main(string[] args)
         {
             double a = 1;
-            double b = -5;
-            double c = 6;
+            double b = 0;
+            double c = 0;
 
             var rozw = PierwiastekRownaniaKw.Solve(a, b, c);
             if (double.IsNaN(rozw.Item1))
