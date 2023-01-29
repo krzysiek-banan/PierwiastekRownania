7d2682e78893b48eff8c7251f413356632431e5e
krzysiek-banan <84676694+krzysiek-banan@users.noreply.github.com>
2023-01-29 15:07:47 +0100

    v1

--- Program.cs
+++ Program.cs
@@ -2,11 +2,40 @@
 
 namespace PierwiastekRownania
 {
-    class Program
+    class PierwiastekRownaniaKw
     {
+        public static Tuple<double, double> Solve(double a, double b, double c)
+        {
+            var discriminant = b * b - 4 * a * c;
+            if (discriminant < 0)
+            {
+                return Tuple.Create(double.NaN, double.NaN);
+            }
+            else if (discriminant == 0)
+            {
+                return Tuple.Create(-b / (2 * a), double.NaN);
+            }
+            else
+            {
+                var sqrtDicriminant = Math.Sqrt(discriminant);
+                return Tuple.Create((-b + sqrtDicriminant) / (2 * a), (-b - sqrtDicriminant) / (2 * a));
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a = 1;
+            double b = -5;
+            double c = 6;
+
+            var rozw = PierwiastekRownaniaKw.Solve(a, b, c);
+            if (double.IsNaN(rozw.Item1))
+            {
+                Console.WriteLine("Równanie nie posiada pierwiastków rzeczywistych");
+            }
+            else
+            {
+                Console.WriteLine($"Pieriastki równania to {rozw.Item1} oraz {rozw.Item2}");
+            }
         }
     }
 }
